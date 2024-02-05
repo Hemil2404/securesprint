dfabacff05fd013758ef2db03589bff5803f951f
Hemil2404 <83370518+Hemil2404@users.noreply.github.com>
2024-02-05 10:00:07 -0500

    SS::changes

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 st.title('Phishing Website Detection using Machine Learning')
 st.write('This ML-based app is developed for educational purposes. Objective of the app is detecting phishing websites only using content data. Not URL!'
-         ' You can see the details of approach, data set, and feature set if you click on _"See The Details"_. ')
+         ' You can see the details of approach, data set, and feature set if you click on "See The Details". ')
 
 
 with st.expander("PROJECT DETAILS"):
