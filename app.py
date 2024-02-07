aaab64b33153ee31adfac576ca6f3cdc941b29ab
Hemil2404 <83370518+Hemil2404@users.noreply.github.com>
2024-02-07 09:33:34 -0500

    changes

--- app.py
+++ app.py
@@ -14,34 +14,8 @@ st.write('This ML-based app is developed for educational purposes. Objective of 
 
 with st.expander("PROJECT DETAILS"):
     st.subheader('Approach')
-    st.write('I used _supervised learning_ to classify phishing and legitimate websites. '
-             'I benefit from content-based approach and focus on html of the websites. '
-             'Also, I used scikit-learn for the ML models.'
-             )
-    st.write('For this educational project, '
-             'I created my own data set and defined features, some from the literature and some based on manual analysis. '
-             'I used requests library to collect data, BeautifulSoup module to parse and extract features. ')
-    st.write('The source code and data sets are available in the below Github link:')
-    st.write('_https://github.com/emre-kocyigit/phishing-website-detection-content-based_')
+  
 
-    st.subheader('Data set')
-    st.write('I used _"phishtank.org"_ & _"tranco-list.eu"_ as data sources.')
-    st.write('Totally 26584 websites ==> **_16060_ legitimate** websites | **_10524_ phishing** websites')
-    st.write('Data set was created in October 2022.')
-
-    # ----- FOR THE PIE CHART ----- #
-    labels = 'phishing', 'legitimate'
-    phishing_rate = int(ml.phishing_df.shape[0] / (ml.phishing_df.shape[0] + ml.legitimate_df.shape[0]) * 100)
-    legitimate_rate = 100 - phishing_rate
-    sizes = [phishing_rate, legitimate_rate]
-    explode = (0.1, 0)
-    fig, ax = plt.subplots()
-    ax.pie(sizes, explode=explode, labels=labels, shadow=True, startangle=90, autopct='%1.1f%%')
-    ax.axis('equal')
-    st.pyplot(fig)
-    # ----- !!!!! ----- #
-
-    st.write('Features + URL + Label ==> Dataframe')
     st.markdown('label is 1 for phishing, 0 for legitimate')
     number = st.slider("Select row number to display", 0, 100)
     st.dataframe(ml.legitimate_df.head(number))
@@ -86,8 +60,7 @@ with st.expander('EXAMPLE PHISHING URLs:'):
 
 choice = st.selectbox("Please select your machine learning model",
                  [
-                     'Gaussian Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Random Forest',
-                     'AdaBoost', 'Neural Network', 'K-Neighbours'
+                     'Decision Tree', 'Neural Network', 'K-Neighbours'
                  ]
                 )
 
